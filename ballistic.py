8a441ee2f579d64de102f9ca1bb8da4a4bdc87c5
tug48516 <35577358+tug48516@users.noreply.github.com>
2018-02-13 14:43:36 -0500

    Add files via upload
    with arrays

--- ballistic.py
+++ ballistic.py
@@ -8,17 +8,27 @@
 #HP=highest point
 #Distance to ground =DG
 #Flight time=FT
+#y velo=22.5, x velo=39.0
 
-
-
+import numpy
 import math as ma
-g=9.8
+import matplotlib
+import matplotlib.pyplot as plt
+
+g=-9.8
 IH=10
 IVM=45
-t=0.1
+t=0.5
 IA=ma.pi/6.
 HP=((45**2.)*ma.sin(IA))/(2*g)
 DG=HP+IH
 FT=(2*IVM*ma.sin(IA))/g
 
-print("highest point",HP,"Distance to Grond",DG,"Flight time",FT)
+
+arr=numpy.array([0.2,0.4,0.6,0.8,1.0,1.2,1.4,1.6,1.8,2.0])
+y=22.5+g*arr
+x=39*arr
+
+plt.figure()
+plt.scatter(x,y)
+plt.show()
